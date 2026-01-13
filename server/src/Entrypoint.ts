c0a16b41393155da05156104f8966b97918c529a
roney ruiz <roneyruizrojas@gmail.com>
2026-01-12 22:03:26 -0600

    feat: create a simple rest api

--- server/src/Entrypoint.ts
+++ server/src/Entrypoint.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, World!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
