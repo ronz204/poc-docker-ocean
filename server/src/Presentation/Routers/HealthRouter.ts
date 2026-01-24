d928c6bbf4de0a561016b625d702b8b599bb78b8
roney ruiz <roneyruizrojas@gmail.com>
2026-01-23 21:57:56 -0600

    chore: purge bad named files

--- server/src/Presentation/Routers/HealthRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia, type ElysiaConfig } from "elysia";
-
-const config: ElysiaConfig<"/health"> = {
-  prefix: "/health", name: "health-router"
-};
-
-export const HealthRouter = new Elysia(config)
-  .get("/health", () => ({ status: "healthy" }));
