025bd3f3bedb87b22ec2958218aface6eb6f54ed
roney ruiz <roneyruizrojas@gmail.com>
2026-03-23 17:04:43 -0600

    feat(elysia): adjust env variables

--- server/source/entry.ts
+++ server/source/entry.ts
@@ -3,7 +3,7 @@ import { Elysia } from "elysia";
 const app = new Elysia()
   .get("/", () => ({ ping: "pong" }))
   .get("/health", () => ({ status: "healthy" }))
-  .listen(3000);
+  .listen(process.env.PORT || 3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
 console.log(`🦊 Elysia is running at ${url}`);
