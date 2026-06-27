8e72a1f6ad85deae724b1709ecce55f3b8341fd9
roney ruiz <roneyruizrojas@gmail.com>
2026-06-26 18:37:44 -0600

    chore(infra): purge server and docker compose

--- server/source/entry.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from "elysia";
-
-const app = new Elysia()
-  .get("/", () => ({ ping: "pong" }))
-  .get("/health", () => ({ status: "healthy" }))
-  .listen(process.env.PORT || 3000);
-
-const url = `http://${app.server?.hostname}:${app.server?.port}`;
-console.log(`🦊 Elysia is running at ${url}`);
